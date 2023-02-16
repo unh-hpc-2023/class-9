85d9f9b54da78d55d180e60e0eb4a71f4cd21b36
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-02-15 23:33:33 -0500

    class8s: verify that we can add 4-component vectors now

--- c/test_vector_add.c
+++ c/test_vector_add.c
@@ -10,12 +10,22 @@
 
 int main(int argc, char** argv)
 {
-  double x[N] = {1., 2., 3.};
-  double y[N] = {2., 3., 4.};
-  double z[N];
+  {
+    double x[N] = {1., 2., 3.};
+    double y[N] = {2., 3., 4.};
+    double z[N];
 
-  vector_add(x, y, z, N);
-  assert(z[0] == 3. && z[1] == 5. && z[2] == 7.);
+    vector_add(x, y, z, N);
+    assert(z[0] == 3. && z[1] == 5. && z[2] == 7.);
+  }
+
+  {
+    double x[4] = {1., 2., 3., 4.};
+    double y[4] = {2., 3., 4., 5.};
+    double z[4];
+    vector_add(x, y, z, 4);
+    assert(z[0] == 3. && z[1] == 5. && z[2] == 7. && z[3] == 9.);
+  }
 
   return 0;
 }
