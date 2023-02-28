a33db28a8ec7e3a8187186797859627d08c2c439
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-02-28 10:13:39 -0500

    cxx: use new, delete for allocation/deallocation

--- cxx/matrix.cxx
+++ cxx/matrix.cxx
@@ -2,18 +2,17 @@
 #include "linear_algebra.h"
 
 #include <stdio.h>
-#include <stdlib.h>
 
 void matrix_construct(struct matrix* A, int n_rows, int n_cols)
 {
-  A->data = (double*)calloc(n_rows * n_cols, sizeof(*A->data));
+  A->data = new double[n_rows * n_cols];
   A->n_rows = n_rows;
   A->n_cols = n_cols;
 }
 
 void matrix_destruct(struct matrix* A)
 {
-  free(A->data);
+  delete[] A->data;
   // The below isn't necessary, but may help make certain bugs more obvious.
   // A->data = NULL;
 }
--- cxx/vector.cxx
+++ cxx/vector.cxx
@@ -2,17 +2,16 @@
 #include "linear_algebra.h"
 
 #include <stdio.h>
-#include <stdlib.h>
 
 void vector_construct(struct vector* v, int n)
 {
-  v->data = (double*)calloc(n, sizeof(*v->data));
+  v->data = new double[n];
   v->n = n;
 }
 
 void vector_destruct(struct vector* v)
 {
-  free(v->data);
+  delete[] v->data;
   // The below isn't necessary, but may help make certain bugs more obvious.
   // v->data = NULL;
   // v->n = 0;
