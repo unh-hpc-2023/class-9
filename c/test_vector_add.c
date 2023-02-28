860f25dc2b94270c5edc0c81c8ac20a2806d69bc
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-02-28 10:04:39 -0500

    class9s: consolidate test_vector_add tests
    Now that our test is written in a dimension-independent way, we can
    avoid the duplication.

--- c/test_vector_add.c
+++ c/test_vector_add.c
@@ -9,52 +9,33 @@
 //
 // test the vector_add() function
 
+void test_vector_add(int N)
+{
+  struct vector x, y, z, z_ref;
+  vector_construct(&x, N);
+  vector_construct(&y, N);
+  vector_construct(&z, N);
+  vector_construct(&z_ref, N);
+
+  for (int i = 0; i < N; i++) {
+    VEC(&x, i) = 1 + i;
+    VEC(&y, i) = 2 + i;
+    VEC(&z_ref, i) = 3 + 2 * i;
+  }
+
+  vector_add(&x, &y, &z);
+  assert(vector_is_equal(&z, &z_ref));
+
+  vector_destruct(&x);
+  vector_destruct(&y);
+  vector_destruct(&z);
+  vector_destruct(&z_ref);
+}
+
 int main(int argc, char** argv)
 {
-  {
-    const int N = 3;
-    struct vector x, y, z, z_ref;
-    vector_construct(&x, N);
-    vector_construct(&y, N);
-    vector_construct(&z, N);
-    vector_construct(&z_ref, N);
-
-    for (int i = 0; i < N; i++) {
-      VEC(&x, i) = 1 + i;
-      VEC(&y, i) = 2 + i;
-      VEC(&z_ref, i) = 3 + 2 * i;
-    }
-
-    vector_add(&x, &y, &z);
-    assert(vector_is_equal(&z, &z_ref));
-
-    vector_destruct(&x);
-    vector_destruct(&y);
-    vector_destruct(&z);
-    vector_destruct(&z_ref);
-  }
-
-  {
-    const int N = 4;
-    struct vector x, y, z, z_ref;
-    vector_construct(&x, N);
-    vector_construct(&y, N);
-    vector_construct(&z, N);
-    vector_construct(&z_ref, N);
-
-    for (int i = 0; i < N; i++) {
-      VEC(&x, i) = 1 + i;
-      VEC(&y, i) = 2 + i;
-      VEC(&z_ref, i) = 3 + 2 * i;
-    }
-
-    vector_add(&x, &y, &z);
-    assert(vector_is_equal(&z, &z_ref));
-
-    vector_destruct(&x);
-    vector_destruct(&y);
-    vector_destruct(&z);
-  }
+  test_vector_add(3);
+  test_vector_add(4);
 
   return 0;
 }
